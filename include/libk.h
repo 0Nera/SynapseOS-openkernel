2b5103ed612297f138f722d197eb76dce2bdd95c
Aren Elchinyn <slizen.can@gmail.com>
2022-10-19 20:11:53 +0300

    [фича] Поддержка va_start, va_end, va_arg, va_copy

--- include/libk.h
+++ include/libk.h
@@ -16,6 +16,12 @@
 
 #define noreturn _Noreturn
 
+#define va_start(v,l)	__builtin_va_start(v,l)
+#define va_end(v)	__builtin_va_end(v)
+#define va_arg(v,l)	__builtin_va_arg(v,l)
+#define va_copy(d,s)	__builtin_va_copy(d,s)
+#define __va_copy(d,s)	__builtin_va_copy(d,s)
+
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
