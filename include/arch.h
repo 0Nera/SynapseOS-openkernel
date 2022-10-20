70b2660b3208cd9ccf301104746dd36403b13b67
Aren Elchinyn <slizen.can@gmail.com>
2022-10-20 14:35:18 +0300

    [фича] Функция rdtsc для вычисления времени выполнения

--- include/arch.h
+++ include/arch.h
@@ -22,4 +22,22 @@ unsigned int arch_get_kernel_size();
 #endif
 
 
+#if defined(__i386__)
+
+static inline unsigned long long rdtsc(void) {
+    unsigned long long int x;
+    asm volatile (".byte 0x0f, 0x31" : "=A" (x));
+    return x;
+}
+
+#elif defined(__x86_64__)
+
+static inline unsigned long long rdtsc(void) {
+    unsigned hi, lo;
+    asm volatile ("rdtsc" : "=a"(lo), "=d"(hi));
+    return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
+}
+
+#endif
+
 #endif // arch.h
