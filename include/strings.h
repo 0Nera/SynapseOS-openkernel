168d362bfb83c909f03f015ce91eea9473bacc8d
Aren Elchinyn <slizen.can@gmail.com>
2022-10-24 21:46:27 +0300

    [фича] Инициализация типа для UTF8 строк

---
+++ include/strings.h
@@ -0,0 +1,23 @@
+/**
+ * @file strings.h
+ * @author Арен Елчинян (a2.dev@yandex.com)
+ * @brief Функции для работы со строками
+ * @version 0.1.0
+ * @date 24-10-2022
+ * 
+ * @copyright Арен Елчинян (c) 2022
+ * 
+ */
+
+
+#include <libk.h>
+
+
+#ifndef	_STRINGS_H
+#define	_STRINGS_H	1
+
+
+typedef int8_t string_utf8_t;
+
+
+#endif // strings.h
